de22c4f6b4c791e35f4d7002ca209af93d5349cb
Ravina Vala <vala8460@gmail.com>
2023-02-21 19:15:21 -0500

    Setup NPM scripts and webpack to use react, set header for main plugin file

---
+++ react-faq-plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ Plugin Name: Faq Plugin
+ Description: A plugin that adds a searchable and collapsible FAQ section to your website using React JS.
+ Version: 1.0.0
+ Author: Ravina Vala
+ License: GPL2
+ License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ Text Domain: faq-section
+ */
+
+
+

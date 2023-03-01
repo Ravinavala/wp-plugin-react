6328c426c7fd035c8abe69ab7cc8b58025f46afe
Ravina Vala <vala8460@gmail.com>
2023-03-01 00:00:48 -0500

    add shortcode on setting page so user can use any post/page

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'adea3c9cfcdc3c2026fb');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '7a1050cc6fc5700b82b0');
--- src/components/Faqlist.js
+++ src/components/Faqlist.js
@@ -1,32 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import classes from "./FaqList.module.css"
-
-const FaqList = () => {
+    const FaqList = () => {
     const [faqList, setFaqList] = useState([]);
 
     useEffect(() => {
         fetch('/litch_venture/wp-json/faqplugin/v1/getfaq').then((response) => {
             return response.json();
         }).then((data) => {
-
             console.log(data)
             setFaqList(data);
-
         }
         )
-    },[]);
+    }, []);
 
     return (
         <div className={classes.faq_main}>
-        <h2>Frequently Asked Questions</h2>
-        {faqList.map((faqItem) => 
-            <div key={faqItem.id} className={classes.faq_item}>
-                <div className={classes.faq_question}>{faqItem.question}</div>
-                <div className={classes.faq_answer}>{faqItem.answer}</div>
-            </div>
-        )}
-      </div>
-     );
+            <h2>Frequently Asked Questions</h2>
+            {faqList.map((faqItem) =>
+                <div key={faqItem.id} className={classes.faq_item}>
+                    <div className={classes.faq_question}>{faqItem.question}</div>
+                    <div className={classes.faq_answer}>{faqItem.answer}</div>
+                </div>
+            )}
+        </div>
+        );
 }
 
 export default FaqList;
